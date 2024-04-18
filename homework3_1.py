f3b0a3d01afda9e09530047e1e3c58f64c055204
serafim <astudzinky@gmail.com>
2024-04-18 15:46:03 +0200

    changed: formatting

--- homework3_1.py
+++ homework3_1.py
@@ -2,8 +2,8 @@ from datetime import datetime
 
 
 def get_days_from_today(date):
-    DATE_FORMAT = "%Y-%m-%d"   
-    input_date = None 
+    DATE_FORMAT = "%Y-%m-%d"
+    input_date = None
     try:
         input_date = datetime.strptime(date, DATE_FORMAT)
     except TypeError:
@@ -11,7 +11,8 @@ def get_days_from_today(date):
               "but should be a string, date format is \"YYYY-MM-DD\" e.g \"2021-11-23\"")
     except ValueError:
         print(f"Input paramentr {date} has wrong date format."
-              " Date format should be \"YYYY-MM-DD\" e.g \"2021-11-23\"") 
+              " Date format should be \"YYYY-MM-DD\" e.g \"2021-11-23\"")
     return datetime.now().toordinal()-input_date.toordinal()
-    
-print (get_days_from_today("2024-04-17"))
+
+
+print(get_days_from_today("2024-04-17"))
